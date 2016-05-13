0b9d03357febbdd6832bec93e4e6c89a774d5b64
alankis <alan.kish38@gmail.com>
2016-05-13 21:47:32 +0200

    Add CSV reader and test CSV file

--- book_in_stock.rb
+++ book_in_stock.rb
@@ -1,24 +1,10 @@
 # BookInStock Class Defintion
 
 class BookInStock
-  attr_reader :isbn
-  attr_accessor :price
+  attr_reader :isbn, :price
 
   def initialize(isbn, price)
     @isbn = isbn
-    @price = price
-  end
-
-  def to_s
-    "ISBN: #{@isbn}, price #{@price}"
-  end
-
-  # Virtual Attribute 
-  def price_in_cents
-    Integer(price*100 + 0.5)
-  end
-
-  def price_in_cents=(cents) 
-    @price = cents/100
+    @price = Float(price)
   end
 end
